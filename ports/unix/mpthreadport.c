afe519d7e00d2ab1578ec8a5173530801fd92056
David Lechner <david@pybricks.com>
2020-01-22 17:22:17 -0600

    py/modthread: start_new_thread CPython compatibility
    In CPython, _thread.start_new_thread() returns an ID that is the same
    ID that is returned by _thread.get_ident(). The current MicroPython
    implementation of _thread.start_new_thread() always returns None.
    
    This adds a CPython compatible implementation using the
    MICROPYTHON_CPYTHON_COMPAT guard so that we don't break existing users.
    It also provides an implementation for the unix port.

--- ports/unix/mpthreadport.c
+++ ports/unix/mpthreadport.c
@@ -191,6 +191,12 @@ void mp_thread_set_state(mp_state_thread_t *state) {
     pthread_setspecific(tls_key, state);
 }
 
+#if MICROPY_CPYTHON_COMPAT
+mp_uint_t mp_thread_get_id(void) {
+    return pthread_self();
+}
+#endif
+
 void mp_thread_start(void) {
     // enable realtime priority if `-X realtime` command line parameter was set
     #if defined(__APPLE__)
@@ -210,7 +216,12 @@ void mp_thread_start(void) {
     mp_thread_unix_end_atomic_section();
 }
 
-void mp_thread_create(void *(*entry)(void *), void *arg, size_t *stack_size) {
+#if MICROPY_CPYTHON_COMPAT
+mp_uint_t
+#else
+void
+#endif
+mp_thread_create(void *(*entry)(void *), void *arg, size_t *stack_size) {
     // default stack size is 8k machine-words
     if (*stack_size == 0) {
         *stack_size = 8192 * sizeof(void *);
@@ -265,7 +276,12 @@ void mp_thread_create(void *(*entry)(void *), void *arg, size_t *stack_size) {
 
     mp_thread_unix_end_atomic_section();
 
+    #if MICROPY_CPYTHON_COMPAT
+    MP_STATIC_ASSERT(sizeof(mp_uint_t) == sizeof(pthread_t));
+    return id;
+    #else
     return;
+    #endif
 
 er:
     mp_raise_OSError(ret);
