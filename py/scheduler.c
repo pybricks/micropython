657567dd7601250c7a93f8b5134c7f8dff04abb5
David Lechner <david@pybricks.com>
2020-01-23 13:54:37 -0600

    py/mpstate: ensure scheduler only runs on main thread
    This uses MP_STATE_IS_MAIN_THREAD macro to ensure that the scheduler
    only runs on the main thread. This is similar to the way CPython only
    allows pending calls on the main thread (ref: Py_AddPendingCall).

--- py/scheduler.c
+++ py/scheduler.c
@@ -235,7 +235,7 @@ void mp_handle_pending(bool raise_exc) {
 
     // Handle any pending callbacks.
     #if MICROPY_ENABLE_SCHEDULER
-    if (MP_STATE_VM(sched_state) == MP_SCHED_PENDING) {
+    if (mp_thread_is_main_thread() && MP_STATE_VM(sched_state) == MP_SCHED_PENDING) {
         mp_sched_run_pending();
     }
     #endif
