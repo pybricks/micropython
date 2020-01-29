ade80e43ea12ea9e5e61903bb2d4558b0faf223e
David Lechner <david@pybricks.com>
2020-01-29 11:06:42 -0600

    pybricks: redirect stdout to stderr by default
    Since we use graphics mode, text printed to stdout it not visible on
    the EV3 when running with brickrun. The ev3dev VS Code extension
    redirects stderr to the output pane, but not stdout. So we redirect
    stdout to stderr by default so that print() "just works".

--- ports/unix/main.c
+++ ports/unix/main.c
@@ -491,6 +491,12 @@ MP_NOINLINE int main_(int argc, char **argv) {
     #endif
     mp_stack_set_limit(stack_limit);
 
+    if (getenv("PYBRICKS_NO_REDIRECT_STDOUT") == NULL) {
+        if (dup2(STDERR_FILENO, STDOUT_FILENO) == -1) {
+            perror("Failed to redirect stdout to stderr");
+        }
+    }
+
     pre_process_options(argc, argv);
 
     #if MICROPY_ENABLE_GC
