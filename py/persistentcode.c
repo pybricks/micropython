9f0f5959e85488fc8b389ea90e5564242731f290
Laurens Valk <laurens@pybricks.com>
2022-07-26 13:56:29 +0200

    py/persistentcode: Run .mpy from RAM without copying.
    This is a partial, experimental implementation of 8381. We could use
    it until that PR gets merged upstream, or explore something like this
    as a smaller solution since it doesn't require various extmod modules.
    
    This allows us to run an .mpy blob from RAM without making a copy while
    loading it. This way we don't have to use free_len to free up the
    original mpy data, so the user can run it again without re-uploading.

--- py/persistentcode.c
+++ py/persistentcode.c
@@ -133,6 +133,14 @@ void mp_native_relocate(void *ri_in, uint8_t *text, uintptr_t reloc_text) {
 
 #endif
 
+#if MICROPY_VFS_MAP_MINIMAL
+mp_uint_t mp_vfs_map_minimal_readbyte(void *data);
+const uint8_t *mp_vfs_map_minimal_read_bytes(mp_reader_t *reader, size_t len);
+STATIC inline bool reader_is_map(mp_reader_t *reader) {
+    return reader->readbyte == mp_vfs_map_minimal_readbyte;
+}
+#endif
+
 STATIC int read_byte(mp_reader_t *reader) {
     return reader->readbyte(reader->data);
 }
@@ -162,6 +170,11 @@ STATIC qstr load_qstr(mp_reader_t *reader) {
         return len >> 1;
     }
     len >>= 1;
+    #if MICROPY_VFS_MAP_MINIMAL
+    if (reader_is_map(reader)) {
+        return qstr_from_strn_static((const char *)mp_vfs_map_minimal_read_bytes(reader, len + 1), len);
+    }
+    #endif
     char *str = m_new(char, len);
     read_bytes(reader, (byte *)str, len);
     read_byte(reader); // read and discard null terminator
@@ -169,6 +182,23 @@ STATIC qstr load_qstr(mp_reader_t *reader) {
     m_del(char, str, len);
     return qst;
 }
+
+#if MICROPY_VFS_MAP_MINIMAL
+STATIC mp_obj_t mp_obj_new_str_static(const mp_obj_type_t *type, const byte *data, size_t len) {
+    if (type == &mp_type_str) {
+        qstr q = qstr_find_strn((const char *)data, len);
+        if (q != MP_QSTRnull) {
+            return MP_OBJ_NEW_QSTR(q);
+        }
+    }
+    mp_obj_str_t *o = m_new_obj(mp_obj_str_t);
+    o->base.type = type;
+    o->len = len;
+    o->hash = qstr_compute_hash(data, len);
+    o->data = data;
+    return MP_OBJ_FROM_PTR(o);
+}
+#endif
 
 STATIC mp_obj_t load_obj(mp_reader_t *reader) {
     byte obj_type = read_byte(reader);
@@ -197,11 +227,26 @@ STATIC mp_obj_t load_obj(mp_reader_t *reader) {
             }
             return MP_OBJ_FROM_PTR(tuple);
         }
+
         vstr_t vstr;
-        vstr_init_len(&vstr, len);
-        read_bytes(reader, (byte *)vstr.buf, len);
+        #if MICROPY_VFS_MAP_MINIMAL
+        if (reader_is_map(reader)) {
+            vstr.len = len;
+            vstr.buf = (char *)mp_vfs_map_minimal_read_bytes(reader, len);
+        } else
+        #endif
+        {
+            vstr_init_len(&vstr, len);
+            read_bytes(reader, (byte *)vstr.buf, len);
+        }
         if (obj_type == MP_PERSISTENT_OBJ_STR || obj_type == MP_PERSISTENT_OBJ_BYTES) {
             read_byte(reader); // skip null terminator
+            #if MICROPY_VFS_MAP_MINIMAL
+            if (reader_is_map(reader)) {
+                const mp_obj_type_t *t = obj_type == MP_PERSISTENT_OBJ_STR ? &mp_type_str : &mp_type_bytes;
+                return mp_obj_new_str_static(t, (const byte *)vstr.buf, vstr.len);
+            }
+            #endif
             if (obj_type == MP_PERSISTENT_OBJ_STR) {
                 return mp_obj_new_str_from_utf8_vstr(&vstr);
             } else {
@@ -238,11 +283,18 @@ STATIC mp_raw_code_t *load_raw_code(mp_reader_t *reader, mp_module_context_t *co
     #endif
 
     if (kind == MP_CODE_BYTECODE) {
-        // Allocate memory for the bytecode
-        fun_data = m_new(uint8_t, fun_data_len);
-        // Load bytecode
-        read_bytes(reader, fun_data, fun_data_len);
-
+        #if MICROPY_VFS_MAP_MINIMAL
+        if (reader_is_map(reader)) {
+            fun_data = (uint8_t *)mp_vfs_map_minimal_read_bytes(reader, fun_data_len);
+        } else
+        #endif
+        {
+            // Allocate memory for the bytecode
+            uint8_t *data = m_new(uint8_t, fun_data_len);
+            // Load bytecode
+            read_bytes(reader, data, fun_data_len);
+            fun_data = data;
+        }
     #if MICROPY_EMIT_MACHINE_CODE
     } else {
         // Allocate memory for native data and load it
