690875f17838b36511b9b2a2b5c13154a288b76c
David Lechner <david@pybricks.com>
2022-12-19 18:03:12 -0600

    py/mphal: Add mp_hal_stdout_tx_flush().
    This adds a new, optional mp_hal_stdout_tx_flush() function and uses
    it to implement the `flush()` method for `sys.stdout`.

--- py/mphal.h
+++ py/mphal.h
@@ -55,6 +55,10 @@ void mp_hal_stdout_tx_strn(const char *str, size_t len);
 void mp_hal_stdout_tx_strn_cooked(const char *str, size_t len);
 #endif
 
+#ifndef mp_hal_stdout_tx_flush
+void mp_hal_stdout_tx_flush(void);
+#endif
+
 #ifndef mp_hal_delay_ms
 void mp_hal_delay_ms(mp_uint_t ms);
 #endif
