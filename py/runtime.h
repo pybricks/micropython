e073b725be0eb1d614e6ae7c2bcc86f14ffa718e
Damien George <damien@micropython.org>
2023-05-09 11:03:17 +1000

    py: Use nlr jump callbacks to optimise compile/execute functions.
    The changed functions now use less stack, and don't have any issues with
    local variables needing to be declared volatile.
    
    Testing on a PYBv1.0, imports (of .py, .mpy and frozen code) now use 64
    less bytes of C stack per import depth.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- py/runtime.h
+++ py/runtime.h
@@ -57,6 +57,13 @@ typedef struct _mp_arg_t {
     mp_arg_val_t defval;
 } mp_arg_t;
 
+// For use with mp_globals_locals_set_from_nlr_jump_callback.
+typedef struct _nlr_jump_callback_node_globals_locals_t {
+    nlr_jump_callback_node_t callback;
+    mp_obj_dict_t *globals;
+    mp_obj_dict_t *locals;
+} nlr_jump_callback_node_globals_locals_t;
+
 // Tables mapping operator enums to qstrs, defined in objtype.c
 extern const byte mp_unary_op_method_name[];
 extern const byte mp_binary_op_method_name[];
@@ -100,6 +107,8 @@ static inline mp_obj_dict_t *mp_globals_get(void) {
 static inline void mp_globals_set(mp_obj_dict_t *d) {
     MP_STATE_THREAD(dict_globals) = d;
 }
+
+void mp_globals_locals_set_from_nlr_jump_callback(void *ctx_in);
 
 mp_obj_t mp_load_name(qstr qst);
 mp_obj_t mp_load_global(qstr qst);
